00be8c569d49c4312b94890fcb6353044bd6851a
SergeyPavluchenko <s.pavluchenko90@gmail.com>
2022-09-10 18:46:06 +0300

    1

---
+++ js/task-01.js
@@ -0,0 +1,15 @@
+const categoryEl = document.querySelector('.ul#categories');
+console.log(categoryEl.length);
+
+
+
+
+
+
+
+// const headEl = document.querySelector('h2');
+// console.log(headEl.firstChild);
+
+// const liEl = document.querySelector('.item')
+// console.log(liEl.children);
+
