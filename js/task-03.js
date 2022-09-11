cd7741746dd827ed86f183177976920d65c41859
SergeyPavluchenko <s.pavluchenko90@gmail.com>
2022-09-11 19:28:07 +0300

    task 3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,24 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+//  1 варінт
+// const ImgReh = document.querySelector('.gallery');
+// // console.log(ImgReh);
+// ImgReh.style.positionAbsolute;
+// const ImgAddContent = images.map(gallery => 
+//   `<li><img src="${gallery.url}" alt="${gallery.alt}"  width = '100%' height = '100%';></li>;`
+// ).join('');
+// ImgReh.insertAdjacentHTML('afterbegin', ImgAddContent);
+// console.log(ImgAddContent);
+
+// 2 варінт
+
+const imgReh = document.querySelector('.gallery');
+// console.log(imgReh);
+
+const imgAddContent = images.map(({ url, alt }) => {
+  return `<li><img class="galary-item" src="${url}" alt="${alt}"  width = '100%' height = '100%';></li>`
+});
+let markup = imgAddContent.join('');
+imgReh.insertAdjacentHTML('afterbegin', markup);
