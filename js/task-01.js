112d99dd7c4cd71c8ddf2170a1a4a525b0db6dcd
SergeyPavluchenko <s.pavluchenko90@gmail.com>
2022-09-11 15:31:54 +0300

    1

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,6 @@
 const categoryEl = document.querySelector('#categories');
 console.log(`Number of categories: ${categoryEl.children.length}`);
 
-
-
-
-// const headEl = document.querySelector('h2');
-// console.log(`Category: ${headEl.firstChild.textContent}`);
-
 const itemRef = document.querySelectorAll('.item')
 
 itemRef.forEach(item => {
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+
+const listItems = ingredients.map(ingredient => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = `${ingredient}`;
+  itemEl.classList.add('item');
+
+  return itemEl;
+});
+ingredientsEl.append(...listItems);
+console.log(listItems);
