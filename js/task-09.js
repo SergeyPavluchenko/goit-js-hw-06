e9fa897a82814dbbb7af9ddabf050854ea91279b
SergeyPavluchenko <s.pavluchenko90@gmail.com>
2022-09-18 16:01:42 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const changeColorBtnRef = document.querySelector('.change-color');
+const spanRef = document.querySelector('.color');
+const changeBodyColorRef = document.querySelector('body');
+// console.log(changeBodyColorRef);
+
+changeColorBtnRef.addEventListener('click', changeColorBtnOnBody);
+
+function changeColorBtnOnBody() {
+  const currentColor = getRandomHexColor;
+  spanRef.textContent = currentColor,
+  changeBodyColorRef.style.backgroundColor = currentColor;
 }
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
